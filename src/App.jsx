f392ef3cf9ce18c18c381ebe2b05ebb73089952b
“Aniko” <aniko.juhasz@yahoo.com>
2025-03-30 22:28:18 +0200

    Add MainLayout & basic routing with Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import PetDetail from "./pages/PetDetail";
+import Pets from "./pages/Pets";
+import Reminders from "./pages/Reminders";
+import Settings from "./pages/Settings";
 
-function App() {
+import MainLayout from "./layout/MainLayout";
+
+const App = () => {
     return (
-        <>
-            <h1 class="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="auth" element={<Auth />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="pets" element={<Pets />} />
+                    <Route path="pets/:id" element={<PetDetail />} />
+                    <Route path="reminders" element={<Reminders />} />
+                    <Route path="settings" element={<Settings />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
